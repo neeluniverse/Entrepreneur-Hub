9258314687f546ea189eeaa175eeb286c447a6d5
neeluniverse <neel41707@gmail.com>
2025-09-14 22:22:58 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, BooleanField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, Optional
 
 class SignupForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(max=100)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    bio = TextAreaField('Bio', validators=[Length(max=500)])
-    skills = StringField('Skills', validators=[Length(max=200)])
-    avatar_url = StringField('Avatar URL', validators=[Length(max=200)])
+    bio = TextAreaField('Bio', validators=[Optional(), Length(max=500)])
+    skills = StringField('Skills', validators=[Optional(), Length(max=200)])
+    avatar_url = StringField('Avatar URL', validators=[Optional(), Length(max=200)])
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -18,13 +18,10 @@ class LoginForm(FlaskForm):
 
 class PostForm(FlaskForm):
     content = TextAreaField('Content', validators=[DataRequired()])
-    image_url = StringField('Image URL', validators=[Length(max=200)])
+    image_url = StringField('Image URL', validators=[Optional(), Length(max=200)])
 
 class EditProfileForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(max=100)])
-    bio = TextAreaField('Bio', validators=[Length(max=500)])
-    skills = StringField('Skills', validators=[Length(max=200)])
-    avatar_url = StringField('Avatar URL', validators=[Length(max=200)])
-
-class MessageForm(FlaskForm):
-    content = TextAreaField('Message', validators=[DataRequired()])
+    bio = TextAreaField('Bio', validators=[Optional(), Length(max=500)])
+    skills = StringField('Skills', validators=[Optional(), Length(max=200)])
+    avatar_url = StringField('Avatar URL', validators=[Optional(), Length(max=200)])
